d42e3be29d26e753c46d42ea64248b3da2547bfa
EdgarCCosta <esantiagorodrigues@birt.es>
2023-11-24 22:58:24 +0100

    ikolçj.branch commit

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <title>Formulario</title>
     <link rel="stylesheet" href=".\css\styles.css">
- 
-    </style>
 </head>
 <body>
 
@@ -19,28 +17,28 @@
         <br>
         <label for="nombre">Nombre:</label>
         <br>
-        <input type="text" name="nombre" value="Edgar">
+        <input type="text" name="nombre" >
         <br>
 
         <!-- Text input para apellidos -->
         <br>
         <label for="apellidos">Apellidos:</label>
         <br>
-        <input type="text" name="apellidos" value="Costa">
+        <input type="text" name="apellidos" >
         <br>
 
         <!-- Text input para libro -->
         <br>
         <label for="libro">Libro:</label>
         <br>
-        <input type="text" id="libro" name="libro" value="Crow">
+        <input type="text" id="libro" name="libro" >
         <br>
 
         <!-- Email input para email -->
         <br>
         <label for="email">Email:</label>
         <br>
-        <input type="email" id="email" name="email" value="esantiago@birt" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
+        <input type="email" id="email" name="email"  required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
         <br>
 
         <!-- date input para dataalquiler -->
@@ -54,7 +52,7 @@
         <br>
         <label for="dni">DNI:</label>
         <br>
-        <input type="text" id="dni" name="dni" value="11111111h">
+        <input type="text" id="dni" name="dni" >
         <br>
 
         <!-- Submit Button -->
